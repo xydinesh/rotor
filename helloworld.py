d73992341154032ecda605c92f0013d5725d6d14
Dinesh Weerapurage <xydinesh@gmail.com>
2017-07-23 15:55:32 -0600

    Rename file name

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-
-
-def handler(event, context):
-    return {"message": "Hello, World!"}
-
-
-def post_handler(event, context):
-    return {"message": "I should have created something..."}
